a74bcd46bcca1a586654b53716a2fc4c3f2ae35b
gaotianye <15156506755@163.com>
2017-07-18 16:13:56 +0800

    update：应对static中无法使用this对象

--- Threads/src/cn/celloud/demo3/Sys2.java
+++ Threads/src/cn/celloud/demo3/Sys2.java
@@ -6,6 +6,8 @@ package cn.celloud.demo3;
  *
  */
 public class Sys2 implements Runnable {
+	//应对static中无法使用this对象
+	Object obj = new Object();
 	int i = 100;
 	@Override
 	public void run() {
@@ -13,7 +15,7 @@ public class Sys2 implements Runnable {
 		 * 同步的前提：多个线程使用同一个锁，当前锁就是this
 		 * 其次，锁的范围不要过大（影响性能），但是一定要锁住资源
 		 */
-		synchronized (this) {
+		synchronized (obj) {
 			while(i>0){
 				System.out.println(Thread.currentThread().getName()+",count:"+(i--));
 				try {
