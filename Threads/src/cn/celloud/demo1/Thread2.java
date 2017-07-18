f52fb222b10245009d8657b4542b7cb3b9583147
gaotianye <15156506755@163.com>
2017-07-18 15:06:23 +0800

    update：extends Thread，访问当前线程时，只需要用this即可。而implement Runnable，访问当前线程，则需要Thread.currentThread()

--- Threads/src/cn/celloud/demo1/Thread2.java
+++ Threads/src/cn/celloud/demo1/Thread2.java
@@ -3,6 +3,7 @@ package cn.celloud.demo1;
 public class Thread2 extends Thread {
 	@Override
 	public void run() {
-		System.out.println("线程"+Thread.currentThread().getName()+"正在运行中。。。。。");
+//		System.out.println("线程"+Thread.currentThread().getName()+"正在运行中。。。。。");
+		System.out.println("线程"+this.getName()+"正在运行");
 	}
 }
