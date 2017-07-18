cf895590872635db7db6998e33ed26ff7ca9e17b
gaotianye <15156506755@163.com>
2017-07-18 14:06:37 +0800

    update：extends Thread，带着name，但是创建对象时指定name不好使

--- Threads/src/cn/celloud/demo1/ThreadTest.java
+++ Threads/src/cn/celloud/demo1/ThreadTest.java
@@ -21,5 +21,28 @@ public class ThreadTest {
 		Thread2 t4 = new Thread2();
 		t3.start();
 		t4.start();
+		/**
+		 * 普通：
+		 * 线程Thread-4正在执行中
+			线程Thread-5正在执行中
+		   setName：
+		   	线程test1正在执行中
+			线程test2正在执行中
+		 */
+		Thread3 t_name1 = new Thread3();
+		Thread3 t_name2 = new Thread3();
+		t_name1.setName("test1");
+		t_name2.setName("test2");
+		t_name1.start();
+		t_name2.start();
+		/**
+		 * name没有任何效果：
+		 * 线程Thread-6正在执行中
+			线程Thread-7正在执行中
+		 */
+		Thread3 t_name3 = new Thread3("gaotianye");
+		Thread3 t_name4 = new Thread3("yuwei");
+		t_name3.start();
+		t_name4.start();
 	}
 }
