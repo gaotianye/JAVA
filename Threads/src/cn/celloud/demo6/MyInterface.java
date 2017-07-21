c53bcd4d64ddcac9eeedd166687862142ec9e0b6
gaotianye <15156506755@163.com>
2017-07-21 15:47:08 +0800

    delete:删除多余的类

--- Threads/src/cn/celloud/demo6/MyInterface.java
+++
@@ -1,8 +0,0 @@
-package cn.celloud.demo6;
-
-public interface MyInterface {
-	void say();
-	
-	@Deprecated
-	void speak();
-}
--- Threads/src/cn/celloud/demo6/MyTest.java
+++
@@ -1,7 +0,0 @@
-package cn.celloud.demo6;
-
-public class MyTest {
-	public static void main(String[] args) {
-		Tools.speak();
-	}
-}
